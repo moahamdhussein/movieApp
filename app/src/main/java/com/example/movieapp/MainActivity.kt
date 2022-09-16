7d45c7cfb826334ba90d96077b2eb3700623ef03
moahmed hussein <mh95568@gmail.com>
2022-09-16 05:10:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.testToolBar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+    }
+}
